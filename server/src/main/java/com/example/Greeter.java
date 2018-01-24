403d942dbee3ef0b00a8088dabe14e0d3009f0ec
Shamsher Khan <shamsher.khan@synapse.com>
2018-01-24 18:36:23 +0530

    all changes done 3

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,9 @@ public class Greeter {
   }
 
  /** 
-  * @param updating comments here */
-  * @ return 
+  * @param updating comments here
+  * @ return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
